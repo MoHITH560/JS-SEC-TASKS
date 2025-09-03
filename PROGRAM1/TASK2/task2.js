dc23b49acdc89905881edd915fc5b29dbd58056c
MOHITH560 <24bcs133@kgcas.com>
2025-09-03 18:18:24 +0530

    program1

---
+++ PROGRAM1/TASK2/task2.js
@@ -0,0 +1,25 @@
+// MULTIPLICATION OPERATION
+function multiple(){
+    const num1 = parseFloat(document.getElementById("number1").value);
+    const num2 = parseFloat(document.getElementById("number2").value);
+    const total = num1*num2;
+    document.getElementById("output1").innerText ="The  Value multiplication Is :" +total;
+
+}
+
+// DIVISION OPERATION
+
+function divide(){
+    const num1 = parseFloat(document.getElementById("number1").value);
+    const num2 = parseFloat(document.getElementById("number2").value);
+    if(num1 == 0){
+        document.getElementById("output1").innerText ="Zero is not divide";
+    }
+    else{
+        const total = num1/num2;
+    document.getElementById("output1").innerText ="The  Value division Is :" +total;
+    }
+  
+}
+
+
